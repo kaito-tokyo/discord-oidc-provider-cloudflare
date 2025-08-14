2a33eb909b644e7cd8f03d2ca7cc59d80d8e3228
Kaito Udagawa <umireon@gmail.com>
2025-08-14 15:19:33 +0900

    Update test_helpers.ts

--- test/test_helpers.ts
+++ test/test_helpers.ts
@@ -2,8 +2,8 @@ import { env } from 'cloudflare:test';
 
 export const TEST_OIDC_CLIENT_ID = 'oidc-client-id';
 export const TEST_OIDC_REDIRECT_URI = 'http://localhost/callback';
-export const TEST_OIDC_CLIENT_SECRET = 'K6lG3eUQMmo2KAVriCsijKxhdQJgB8jvmGGaNaDFnKEGrZWs7LFmMFwH1vPmwi4J';
-export const TEST_OIDC_CLIENT_SECRET_HASH = '9905ac160a1bc501c24c9a1d998c8054ddf12bfa42faff75374927faeaecde34';
+export const TEST_OIDC_CLIENT_SECRET = 'K6lG3eUQMmo2KAVriCsijKxhdQJgB8jvmGGaNaDFnKEGrZWs7LFmMFwH1vPmwi4J'; // dummy secret
+export const TEST_OIDC_CLIENT_SECRET_HASH = '9905ac160a1bc501c24c9a1d998c8054ddf12bfa42faff75374927faeaecde34'; // dummy secret
 
 export async function setUpOidcClients() {
 	await env.OIDC_CLIENTS.delete(TEST_OIDC_CLIENT_ID);
